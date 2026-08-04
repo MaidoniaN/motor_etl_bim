4f96a99151c0b45e2fc571e01afb1426dae3a253
Alonso Cid <alonso.cid@gmail.com>
2026-08-04 08:25:47 -0400

    Actualizacion

--- scripts/cargar_db.py
+++ scripts/cargar_db.py
@@ -35,7 +35,9 @@ DB_CONFIG = {
 # ruta_json = "data_output/extraccion_completa.json"
 # ruta_json = "data_output/extraccion_completa_Plumb.json"
 # ruta_json = "data_output/extraccion_completa_Mech.json"
-ruta_json = "data_output/extraccion_completa_Elec.json"
+# ruta_json = "data_output/extraccion_completa_Elec.json"
+# ruta_json = "data_output/extraccion_completa_CajaEscala.json"
+ruta_json = "data_output/extraccion_completa_INS5421.json"
 
 
 
